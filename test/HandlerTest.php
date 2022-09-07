53ab1745264ff451d788ed655373f5e3bb703c90
acroziqin <achmadchoirurroziqin@gmail.com>
2022-09-07 13:15:05 +0700

    Handler

---
+++ test/HandlerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace KrisnaBeaute\BelajarPhpMvc;
+
+use Monolog\Handler\ElasticaHandler;
+use Monolog\Handler\ElasticsearchHandler;
+use Monolog\Handler\SlackHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use PHPUnit\Framework\TestCase;
+
+class HandlerTest extends TestCase
+{
+    public function testHandler()
+    {
+        $logger = new Logger(HandlerTest::class);
+
+        $logger->pushHandler(new StreamHandler("php://stderr"));
+        $logger->pushHandler(new StreamHandler(__DIR__ . '/../application.log'));
+//        $logger->pushHandler(new SlackHandler());
+//        $logger->pushHandler(new ElasticsearchHandler())
+
+        self::assertNotNull($logger);
+
+        self::assertEquals(2, sizeof($logger->getHandlers()));
+    }
+
+}
--- test/LoggerTest.php
+++ test/LoggerTest.php
@@ -20,5 +20,4 @@ class LoggerTest extends TestCase
 
         self::assertNotNull($logger);
     }
-
 }
