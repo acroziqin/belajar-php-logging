e6c9dd607892c450a7d6668f60950a8fea261808
acroziqin <achmadchoirurroziqin@gmail.com>
2022-09-07 13:02:07 +0700

    Logger

---
+++ test/LoggerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace KrisnaBeaute\BelajarPhpMvc;
+
+use Monolog\Logger;
+use PHPUnit\Framework\TestCase;
+
+class LoggerTest extends TestCase
+{
+    public function testLogger()
+    {
+        $logger = new Logger("Krisna Beaute");
+
+        self::assertNotNull($logger);
+    }
+
+    public function testLoggerWithName()
+    {
+        $logger = new Logger(LoggerTest::class);
+
+        self::assertNotNull($logger);
+    }
+
+}
